4c125435fb81a0e8151e0470c846930526e0bc1b
unknown <1798444253@qq.com>
2020-02-09 20:17:00 +0800

    .

--- build/check-versions.js
+++ build/check-versions.js
@@ -4,6 +4,7 @@ const semver = require('semver')
 const packageConfig = require('../package.json')
 const shell = require('shelljs')
 
+
 function exec (cmd) {
   return require('child_process').execSync(cmd).toString().trim()
 }
